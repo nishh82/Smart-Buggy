bc565ba53e6b7647ec4a9ee1ac7e332484ff8c8e
abhishek-rma7 <abhishek.rma7@gmail.com>
2024-04-13 09:59:43 -0400

    Update to app!

--- metro.config.js
+++ metro.config.js
@@ -8,8 +8,10 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  */
 const config = {
     resolver: {
-        assetExts: ['obj', 'tflite', 'jpeg', 'png', 'webp', 'jpg'],
-    }
+        assetExts: ['json', 'obj', 'tflite', 'jpeg', 'png', 'webp', 'jpg'],
+        sourceExts: ['json'],
+    },
+
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
